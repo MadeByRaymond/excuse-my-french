36929ec58f32136ed809fb73c5b1fd2dd703afdc
Raymond O <madebyraymond@gmail.com>
2021-12-26 12:53:26 +0100

    Android and IOS app files commit

--- android/app/src/main/java/com/madebyraymond/excusemyfrench/MainApplication.java
+++ android/app/src/main/java/com/madebyraymond/excusemyfrench/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.dooboolab.RNAudioRecorderPlayerPackage;
 import com.zmxv.RNSound.RNSoundPackage;
